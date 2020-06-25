466552255ee827dbb4f3b3e4dd797501594dfb78
Thabang <tsoulo@student.wethinkcode.co.za>
2020-06-25 10:13:52 +0200

    ex03

--- ex03/ft_recursive_power.c
+++ ex03/ft_recursive_power.c
@@ -6,7 +6,7 @@
 /*   By: tsoulo <marvin@42.fr>                      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/06/24 09:41:26 by tsoulo            #+#    #+#             */
-/*   Updated: 2020/06/25 09:22:01 by tsoulo           ###   ########.fr       */
+/*   Updated: 2020/06/25 10:13:11 by tsoulo           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -26,9 +26,5 @@ int ft_recursive_power(int nb, int power)
 		return (1);
 	}
 }
-int main(void)
-{
- printf("Results is = %i\n",ft_recursive_power(2,3));
- return(0);
-}
+
 
